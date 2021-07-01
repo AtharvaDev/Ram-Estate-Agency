2815bb7134c86f4bd5ee14d6d13a2c6e329d291f
Atharva <atharva1525@gmail.com>
2021-07-02 00:58:54 +0530

    react-router-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Project from "./components/Project";
+import Post from "./components/Post";
 function App() {
   return (
-    <div className="App">
-     RamEstateAgency
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path="/" exact/>
+        <Route component={About} path="/about"/>
+        <Route component={SinglePost} path="/post/:slug"/>
+        <Route component={Post} path="/post"/>
+        <Route component={Project} path="/project"/>
+
+        <div className="App">RamEstateAgency</div>{" "}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
